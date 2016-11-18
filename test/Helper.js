2ca47ba2d0c8f8a7645325b1d6887d81edf9031b
zoltan.mihalyi <zoltan.mihalyi@webvalto.hu>
2016-11-18 14:39:58 +0100

    line separator fix

--- test/Helper.js
+++ test/Helper.js
@@ -3,5 +3,6 @@ var optimize = require('../dist/optimize');
 var assert = require('assert');
 
 module.exports.assertMatch = function ensureMatch(filename) {
-    assert.equal(optimize(fs.readFileSync(`${__dirname}/samples/${filename}.js`, 'UTF-8')), fs.readFileSync(__dirname + '/samples/' + filename + '.optimized.js', 'UTF-8'));
+    var expected = fs.readFileSync(__dirname + '/samples/' + filename + '.optimized.js', 'UTF-8').replace(/\r/g, '');
+    assert.equal(optimize(fs.readFileSync(`${__dirname}/samples/${filename}.js`, 'UTF-8')), expected);
 };
